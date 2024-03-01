22c1b3476ba1044c860970fb797f1f74cf377337
Ramiro Sanchez <109224987+Rancho91@users.noreply.github.com>
2024-03-01 11:42:33 -0300

    Limpie app componentes comentados

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import SignIn  from './modules/Login/SingnIn'
-import Register  from './modules/Login/Register'
-import NavGlobal from './modules/Nav/NavGlobal'
 import Inicio from './modules/Inicio/Inicio'
-import GoogleButton from './modules/Login/GoogleAuthButton/GoogleButton'
 
 function App() {
 
   return (
     <>
-      {/* <SignIn/> */}
-      {/* <Register/> */}
       <Inicio/>
-      {/* <GoogleButton/> */}
     </>
   );
 }

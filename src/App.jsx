722fd3bf27aae8c68fc588868ffebbb05cfe6364
Ramiro Sanchez <109224987+Rancho91@users.noreply.github.com>
2024-03-04 07:51:51 -0300

    armado router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import Inicio from './modules/Inicio/Inicio'
+import React from "react";
+import Inicio from "./modules/Inicio/Inicio";
+import { Route, Router, Routes } from "react-router-dom";
+import CategoriesGrid from "./components/CategoriesGrid";
+import NavGlobal from "./modules/Nav/NavGlobal";
+import Register from "./modules/Login/Register";
+import LoginCard from "./modules/Login/LoginCard/LoginCard";
 
 function App() {
-
   return (
     <>
-      <Inicio/>
+      <NavGlobal />
+      <Routes>
+        {/* Ruta principal */}
+        <Route path="/" element={<Inicio />}></Route>
+
+        {/* Ruta de registro */}
+        <Route path="register" element={<Register />} />
+
+        {/* Ruta de proveedores (visitante) */}
+        <Route path="proveedores" element={<LoginCard />} />
+
+        {/* Ruta de proveedores (visitante) */}
+        <Route path="login" element={<LoginCard />} />
+
+        {/* Ruta de proveedores (dashboard) */}
+        <Route path="miPerfil">
+          <Route path="misProductos" element={""} />
+          <Route path="nuevoProducto" element={""} />
+        </Route>
+
+        {/* Ruta de administrador (dashboard) */}
+        <Route path="administrador">
+          <Route index element={<LoginCard />} />
+          <Route path="proveedores" element={<CategoriesGrid />} />
+          <Route path="publicaciones" element={<Register />} />
+        </Route>
+      </Routes>
     </>
   );
 }

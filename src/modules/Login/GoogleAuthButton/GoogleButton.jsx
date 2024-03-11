c77bd555af70e092710cc85dc73878098bf3b5af
Yael Correa <yscorrea10@gmail.com>
2024-03-11 19:01:23 -0300

    integración con backend en proceso

--- src/modules/Login/GoogleAuthButton/GoogleButton.jsx
+++ src/modules/Login/GoogleAuthButton/GoogleButton.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { useGoogleLogin } from '@react-oauth/google';
+import axios from "axios";
 import google from '../../../assets/svg/google_icon.svg'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function GoogleButton() {
+  const navigate = useNavigate()
 
-    const login = useGoogleLogin({
-        onSuccess: tokenResponse => console.log(tokenResponse),
-        onError: () => {console.log('Fallo del Login')},
-        flow: 'auth-code',
-    });
+    const googleSuccess = async (res) => {
+      const result = res?.scope
+      const token = res?.code
+      try {
+        console.log(res)
+        console.log(token)
+        console.log(result)
+        await axios.post('', { token, result }); // ruta del backend 
+        navigate('/');
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const googleFailure = (error) => {
+      console.log(error)
+      console.log('Error al iniciar sesión. Intente de nuevo más tarde.')
+    }
+
+
+     const login = useGoogleLogin({
+       onSuccess: googleSuccess,
+       onError: googleFailure,
+       flow: 'auth-code',
+   });
       
     return(
           <button onClick={() => login()}>

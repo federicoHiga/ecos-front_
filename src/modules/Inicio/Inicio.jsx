8cd345150482725c920996a472f1c6fad9ca541f
Leandro Mercado <leandro.mercado@alumnos.frm.utn.edu.ar>
2024-03-11 18:52:28 -0300

    Cambios en la pantalla de inicio, se agregan vistas para los resultados de una búsqueda y por si no hay ningún resultado, cambios en el archivo theme.js, se agregaron algunos colores y botones estilados, cambios en la grilla de categorías

--- src/modules/Inicio/Inicio.jsx
+++ src/modules/Inicio/Inicio.jsx
@@ -5,11 +5,16 @@ import ImpactEnterprises from "../../components/ImpactEnterprises";
 import CategoriesGrid from "../../components/CategoriesGrid";
 import Searchbar from "../../components/Searchbar";
 import useGetByNameHook from "../../utils/services/hooks/getHooks";
+import { Typography, useTheme } from "@mui/material";
+import { NoResultsCard, SearchResultCard } from "../../components/SearchResultsCards";
+import { SuppliersData } from "../../utils/data/suppliersMock";
 
 export default function Inicio() {
+  const theme = useTheme();
   const [searchText, setSearchText] = useState(null);
+
   const handleSearch = (evt) => {
-    if(evt == "") setSearchText(null)
+    if (evt == "") setSearchText(null);
     else setSearchText(evt);
   };
 
@@ -18,40 +23,54 @@ export default function Inicio() {
     name: searchText,
   });
 
+  /* Para ver la card de no resultados, quitar el comentario en la condición  */
 
-
-  if(data?.length===0){
-    return(
-      <div>
-         <div id="inicio">
-              <div id="title">
-                <Searchbar functionText={handleSearch} text={searchText} />
-                <p>RED DE IMPACTO</p>
-                <h1>
-                  Conectamos proveedores y personas comprometidas con el impacto y
-                  el consumo consciente viene con error
-                </h1>
-              </div>
-            </div>
+  if (data?.length === 0 /* || data === null */) {
+    return (
+      <div className="search-results-screen">
+        <Searchbar functionText={handleSearch} text={searchText} />
+        <section className="search-results-title-container">
+          <Typography
+            variant="h1"
+            className="search-results-title"
+            sx={{
+              fontFamily: theme.typography.fontFamily,
+              fontWeight: theme.typography.titulos.fontWeight,
+            }}
+          >
+            Resultados de tu búsqueda
+          </Typography>
+        </section>
+        <section className="no-results-container">
+          <NoResultsCard />
+        </section>
       </div>
-    )
+    );
   }
-
-  if(data?.length >0 && data !==null){
-    return(
-      <div>
-        <div id="inicio">
-              <div id="title">
-                <Searchbar functionText={handleSearch} text={searchText} />
-                <p>RED DE IMPACTO</p>
-                <h1>
-                  Conectamos proveedores y personas comprometidas con el impacto y
-                  el consumo consciente viene con data
-                </h1>
-              </div>
-            </div>
+  // condición original => data?.length > 0 && data !== null
+  if (data === null) {
+    return (
+      <div className="search-results-screen">
+        <Searchbar functionText={handleSearch} text={searchText} />
+        <section className="search-results-title-container">
+          <Typography
+            variant="h1"
+            className="search-results-title"
+            sx={{
+              fontFamily: theme.typography.fontFamily,
+              fontWeight: theme.typography.titulos.fontWeight,
+            }}
+          >
+            Resultados de tu búsqueda
+          </Typography>
+        </section>
+        <section className="search-results-grid">
+          {SuppliersData.map((data) => (
+            <SearchResultCard key={data.name} supplier={data} />
+          ))}
+        </section>
       </div>
-    )
+    );
   }
   return (
     <div>
@@ -60,8 +79,8 @@ export default function Inicio() {
           <Searchbar functionText={handleSearch} text={searchText} />
           <p>RED DE IMPACTO</p>
           <h1>
-            Conectamos proveedores y personas comprometidas con el impacto y
-            el consumo consciente
+            Conectamos proveedores y personas comprometidas con el impacto y el consumo
+            consciente
           </h1>
         </div>
       </div>
@@ -71,4 +90,3 @@ export default function Inicio() {
     </div>
   );
 }
-
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -9,6 +9,7 @@ const theme = createTheme({
       main: "#171616",
     },
     verdes: {
+      main: "#00A364",
       dark: "#366C00",
       medium: "#70D20F",
       light: "#99FF33",
@@ -17,6 +18,32 @@ const theme = createTheme({
       dark: "#DD7200",
       medium: "#F8952C",
       light: "#FDAD58",
+    },
+    violeta: {
+      main: "#4E169D",
+      dark: "#510480",
+      medium: "#a628bf",
+      light: "#e4c0ec",
+    },
+    exito: {
+      main: "#1d9129",
+    },
+    revision: {
+      main: "#b86b11",
+    },
+    rojo: {
+      main: "#bc1111",
+    },
+    blanco: {
+      main: "#FAFAFA",
+    },
+    grises: {
+      dark: "#505050",
+      medium: "#d2d2d2",
+      light: "#EAEAEA",
+    },
+    negro: {
+      main: "#222",
     },
   },
   typography: {
@@ -49,7 +76,51 @@ const theme = createTheme({
     borderRadiusMedium: 8,
     borderRadiusLarge: 16,
   },
-  // Podes agregar más configuraciones del tema aca cuando necesites
+  components: {
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: "purple" },
+          style: {
+            textTransform: "none",
+            border: "none",
+            maxWidth: "200px",
+            maxHeight: "40px",
+            borderRadius: "100px",
+            padding: "10px 24px",
+            fontSize: "16px",
+            color: "white",
+            fontWeight: 700,
+            lineHeight: "20px",
+            backgroundColor: "#4E169D",
+            "&:hover": {
+              backgroundColor: "#4E169D",
+            },
+          },
+        },
+        {
+          props: { variant: "categories" },
+          style: {
+            minWidth: "152px",
+            maxWidth: "328px",
+            minHeight: "64px",
+            maxHeight: "72px",
+            border: "none",
+            borderRadius: "16px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "0 5px",
+            textTransform: "none",
+            backgroundColor: "#EAEAEA",
+            "&:hover": {
+              backgroundColor: "#EAEAEA",
+            },
+          },
+        },
+      ],
+    },
+  },
 });
 
 export default theme;

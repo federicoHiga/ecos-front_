0e86df392f697658dca98c32589fab8109308508
Leandro Mercado <leandro.mercado@alumnos.frm.utn.edu.ar>
2024-03-26 10:00:32 -0300

    Implementación del endpoint que devuelve la lista de categorías, en la landing y en la sección de categorías

--- src/components/CategoriesGrid/index.jsx
+++ src/components/CategoriesGrid/index.jsx
@@ -2,37 +2,33 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
 import { Button, useTheme } from "@mui/material";
-import { CategoriesData } from "../../utils/data/categoriesMock";
 import useGetAll from "../../utils/services/hooks/useGetAll";
 
 const CategoriesGrid = () => {
   const theme = useTheme();
   const navigate = useNavigate();
-  const { data, error, loading } = useGetAll({ url: "category" });
-
-  //TODO recorrer esta data en la vista, una vez que estén terminados los cambios del JSON
-  console.log("data", data?.data);
+  const { data } = useGetAll({ url: "category" });
 
   return (
     <div className="categories-section">
       <h6>Red de Proveedores ECO</h6>
       <h3>Categorías</h3>
       <section className="categories-grid">
-        {CategoriesData.slice(0, 8).map((category) => (
+        {data?.data.slice(0, 8).map((category) => (
           <Button
             variant="categories"
             className="categories-grid-button"
             key={category.id}
             onClick={() =>
-              navigate(`/providers/${category.category}`, { state: { category } })
+              navigate(`/providers/${category?.id}`, { state: { category } })
             }
           >
             <div className="button-inner-wrapper">
               <section className="grid-item-icon-container">
-                <img src={category.img} alt={category.title} />
+                <img src={category?.image} alt={category?.category} />
               </section>
               <h6 className="grid-item-title" style={{ color: theme.palette.negro.main }}>
-                {category.category}
+                {category?.category}
               </h6>
             </div>
           </Button>

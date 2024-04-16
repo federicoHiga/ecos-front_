413b60d102214a1763256f635017158b30af6af7
Federico <higafederico@gmail.com>
2024-04-16 18:24:55 -0300

    card de publicaciones consume las imagenes de la bd

--- src/modules/PostsSection/index.jsx
+++ src/modules/PostsSection/index.jsx
@@ -1,14 +1,7 @@
 import { Link } from "react-router-dom";
 import PostsCard from "../../components/PostsCard/index";
 import "./styles.css";
-import { PostsData } from "../../utils/data/postsMock";
 import useGetToken from "../../utils/services/hooks/useGetToken";
-
-const hardImages = [
-  "https://res.cloudinary.com/dxatwbzff/image/upload/v1710415070/Quinto/1c27a72869b176f8ac7bc5f75f460594_fddnpc.jpg",
-  "https://res.cloudinary.com/dpbuvii9v/image/upload/v1712264504/c1498999f8addebf3e800720a2445865_uzmafz.jpg",
-  "https://res.cloudinary.com/dpbuvii9v/image/upload/v1712264503/ca3817b7e452c7de4602ccf498f7afd1_xfkd2x.png",
-];
 
 function PostsSection() {
   const { data, loading, error } = useGetToken(
@@ -16,16 +9,20 @@ function PostsSection() {
     // "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJoaWdhbWFyYWRvbmFmZWRlcmljb0BnbWFpbC5jb20iLCJyb2xlcyI6IkFETUlOSVNUUkFET1IiLCJpYXQiOjE3MTE0NjU0MzcsImV4cCI6MTcxMTQ2OTAzN30.rG72XsUQ2n3mY4Dh9gWdp3pFuGnsuakj6WFx0chNUR565Ww_KGwh-kL3Xk3MhbVLmWQg4vaN283buozLmalFtA"
   );
 
-  console.log('image',data.imagePublicDtoList)
+  const getImagePaths = (postData) => {
+    return postData.imagePublicDtoList.map((imageObject) => imageObject.path);
+  };
+
   return (
     <section className="posts-section">
       <div className="posts-section-title">
         <h1 className="title">Publicaciones</h1>
         <h2>Impulsando transformaciones</h2>
       </div>
-      {data.slice(0, 3).map((post) => (
-        <PostsCard key={post.id} post={post} images={hardImages} />
-      ))}
+      {data.slice(0, 3).map((post) => {
+        const imagePaths = getImagePaths(post);
+        return <PostsCard key={post.id} post={post} images={imagePaths} />;
+      })}
       <Link to={"/posts"}>
         <button className="posts-section-button">Ir a Publicaciones</button>
       </Link>
--- src/modules/PostsView/index.jsx
+++ src/modules/PostsView/index.jsx
@@ -5,12 +5,6 @@ import SearchByChildren from "../../components/SearchFlexible";
 import { useLocation } from "react-router-dom";
 import useGetToken from "../../utils/services/hooks/useGetToken";
 
-const hardImages = [
-  "https://res.cloudinary.com/dxatwbzff/image/upload/v1710415070/Quinto/1c27a72869b176f8ac7bc5f75f460594_fddnpc.jpg",
-  "https://res.cloudinary.com/dpbuvii9v/image/upload/v1712264504/c1498999f8addebf3e800720a2445865_uzmafz.jpg",
-  "https://res.cloudinary.com/dpbuvii9v/image/upload/v1712264503/ca3817b7e452c7de4602ccf498f7afd1_xfkd2x.png",
-];
-
 export default function PostsView() {
   const { pathname } = useLocation();
 
@@ -18,6 +12,10 @@ export default function PostsView() {
     "publication"
     // "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJoaWdhbWFyYWRvbmFmZWRlcmljb0BnbWFpbC5jb20iLCJyb2xlcyI6IkFETUlOSVNUUkFET1IiLCJpYXQiOjE3MTE0NjU0MzcsImV4cCI6MTcxMTQ2OTAzN30.rG72XsUQ2n3mY4Dh9gWdp3pFuGnsuakj6WFx0chNUR565Ww_KGwh-kL3Xk3MhbVLmWQg4vaN283buozLmalFtA"
   );
+
+  const getImagePaths = (postData) => {
+    return postData.imagePublicDtoList.map((imageObject) => imageObject.path);
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -27,9 +25,10 @@ export default function PostsView() {
     <SearchByChildren>
       <div className="postsView-section">
         <div className="postsView-container">
-          {data?.map((post) => (
-            <PostsCard key={post.id} post={post} images={hardImages} />
-          ))}
+          {data?.map((post) => {
+            const imagePaths = getImagePaths(post);
+            return <PostsCard key={post.id} post={post} images={imagePaths} />;
+          })}
         </div>
       </div>
     </SearchByChildren>
